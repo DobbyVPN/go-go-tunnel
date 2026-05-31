460a3857caa9ef2dd0f9247f74d9515d5d5c4fb7
TrueKotenka <shatokhin.05@mail.ru>
2026-05-31 20:11:05 +0300

    fix ios, add verification for android

--- manager/cgo_static.go
+++ manager/cgo_static.go
@@ -9,14 +9,14 @@ package manager
 // Link against the bundled static library (.a) containing all dependencies.
 
 // iOS
-#cgo ios LDFLAGS: -L${SRCDIR}/../lib/ios/libdobby_bridge.a -framework CoreFoundation -framework Security
+#cgo ios LDFLAGS: ${SRCDIR}/../lib/ios/libdobby_bridge.a -framework CoreFoundation -framework Security
 
 // macOS
-#cgo darwin,!ios LDFLAGS: -L${SRCDIR}/../lib/macos/libdobby_bridge.a -framework CoreFoundation -framework Security
+#cgo darwin,!ios LDFLAGS: ${SRCDIR}/../lib/macos/libdobby_bridge.a -framework CoreFoundation -framework Security
 
 // Linux: Use whole-archive to prevent stripping
-#cgo linux,!android LDFLAGS: -L${SRCDIR}/../lib/linux/libdobby_bridge.a -lpthread -ldl -lc++ -lc++abi -lm
+#cgo linux,!android LDFLAGS: ${SRCDIR}/../lib/linux/libdobby_bridge.a -lpthread -ldl -lc++ -lc++abi -lm
 
-#cgo windows LDFLAGS: -L${SRCDIR}/../lib/windows/libdobby_bridge.a
+#cgo windows LDFLAGS: ${SRCDIR}/../lib/windows/libdobby_bridge.a
 */
 import "C"
