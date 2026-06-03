eb5bb1370f3db3ea5efd1d1ab8146d9e01f936ec
TrueKotenka <shatokhin.05@mail.ru>
2026-06-03 20:49:22 +0300

    add static builds for macOs and Android

--- manager/cgo_static.go
+++ manager/cgo_static.go
@@ -12,10 +12,13 @@ package manager
 #cgo ios LDFLAGS: ${SRCDIR}/../lib/ios/libdobby_bridge.a -framework CoreFoundation -framework Security -framework Foundation -framework Network -framework NetworkExtension -lc++
 
 // macOS
-#cgo darwin,!ios LDFLAGS: ${SRCDIR}/../lib/macos/libdobby_bridge.a -framework CoreFoundation -framework Security -framework Foundation -framework Network -framework NetworkExtension -lc++
+#cgo darwin,!ios LDFLAGS: ${SRCDIR}/../lib/macos/libdobby_bridge.a -framework CoreFoundation -framework Security -framework Foundation -framework Network -framework NetworkExtension -framework SystemConfiguration -lc++
 
 // Linux: Use whole-archive to prevent stripping
 #cgo linux,!android LDFLAGS: ${SRCDIR}/../lib/linux/libdobby_bridge.a -lpthread -ldl -lc++ -lc++abi -lm
+
+// Android
+#cgo android,arm64 LDFLAGS: ${SRCDIR}/../lib/android/arm64-v8a/libdobby_bridge.a -llog -lm -lc++_static -lc++abi
 
 #cgo windows LDFLAGS: ${SRCDIR}/../lib/windows/libdobby_bridge.a
 */
